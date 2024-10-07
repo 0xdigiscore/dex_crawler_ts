1879cb038efdfc25a3a19f08bd951fec055ffb7b
0xdigiscore <0xmassis@gmail.com>
2024-10-07 22:24:31 +0800

    feat: fix scripts

--- scripts/run_gmgn_crawler.sh
+++ scripts/run_gmgn_crawler.sh
@@ -8,7 +8,7 @@ set +a
 while true
 do
     echo "[$(date)] 启动 @dex_crawler/gmgn_crawler dev..."
-    yarn workspace @dex_crawler/gmgn_crawler dev
+    yarn start:gmgn_crawler
     echo "[$(date)] 执行完成，等待 10 分钟..."
     sleep 600 # 600 秒 = 10 分钟
 done
--- scripts/run_gmgn_signal.sh
+++ scripts/run_gmgn_signal.sh
@@ -8,7 +8,7 @@ set +a
 while true
 do
     echo "[$(date)] 启动 @dex_crawler/gmgn_signal dev..."
-    yarn workspace @dex_crawler/gmgn_signal dev
+    yarn start:signal
     echo "[$(date)] 执行完成，等待 2 分钟..."
     sleep 60
 done
