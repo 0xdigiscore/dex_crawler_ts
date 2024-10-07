50940cd7e4ba5cb37e49165950d43b7795d0ee33
0xdigiscore <0xmassis@gmail.com>
2024-10-07 22:16:35 +0800

    feat: update signal

--- scripts/run_gmgn_signal.sh
+++ scripts/run_gmgn_signal.sh
@@ -10,5 +10,5 @@ do
     echo "[$(date)] 启动 @dex_crawler/gmgn_signal dev..."
     yarn workspace @dex_crawler/gmgn_signal dev
     echo "[$(date)] 执行完成，等待 2 分钟..."
-    sleep 120
+    sleep 60
 done
