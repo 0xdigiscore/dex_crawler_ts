89a742e4844a7938d739de82f39c56f3cba2ddda
0xdigiscore <0xmassis@gmail.com>
2024-10-09 22:51:38 +0800

    feat: 添加token 安全检查

--- packages/gecko_crawler/src/main.ts
+++ packages/gecko_crawler/src/main.ts
@@ -1,5 +1,6 @@
 import { TaskScheduler, Task } from './tasks/TaskScheduler.js';
 import updateTokenMetrics from './tasks/updateTokenMetrics.js';
+import updateTokenSecurity from './tasks/UpdateTokenSecurity.js';
 
 // 定义任务
 const updateTokenMetricsTask: Task = {
@@ -8,11 +9,19 @@ const updateTokenMetricsTask: Task = {
   taskFunction: updateTokenMetrics,
 };
 
+const updateTokenSecurityTask: Task = {
+  name: '更新 Token 安全',
+  schedule: '*/2 * * * *', // 每2分钟执行一次
+  taskFunction: updateTokenSecurity,
+};
+
 // 初始化任务调度器
-const taskScheduler = new TaskScheduler(2, 100); // 可根据需要调整并发和最小时间间隔
+const taskScheduler = new TaskScheduler(1, 100); // 可根据需要调整并发和最小时间间隔
 
 // 注册任务
 taskScheduler.registerTask(updateTokenMetricsTask);
+taskScheduler.registerTask(updateTokenSecurityTask);
 
 // 启动调度器
 taskScheduler.start();
+updateTokenSecurity();
