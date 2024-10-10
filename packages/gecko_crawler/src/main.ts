db292c07c229ebf1f29cd274b7b24f3efa546bad
0xdigiscore <0xmassis@gmail.com>
2024-10-10 13:25:22 +0800

    feat: update data

--- packages/gecko_crawler/src/main.ts
+++ packages/gecko_crawler/src/main.ts
@@ -1,6 +1,7 @@
 import { TaskScheduler, Task } from './tasks/TaskScheduler.js';
 import updateTokenMetrics from './tasks/updateTokenMetrics.js';
 import updateTokenSecurity from './tasks/UpdateTokenSecurity.js';
+import updateTokenStats from './tasks/UpdateTokenStats.js';
 
 // 定义任务
 const updateTokenMetricsTask: Task = {
@@ -15,13 +16,21 @@ const updateTokenSecurityTask: Task = {
   taskFunction: updateTokenSecurity,
 };
 
+const updateTokenStatsTask: Task = {
+  name: '更新 Token 统计',
+  schedule: '*/2 * * * *', // 每1分钟执行一次
+  taskFunction: updateTokenStats,
+};
+
 // 初始化任务调度器
-const taskScheduler = new TaskScheduler(2, 100); // 可根据需要调整并发和最小时间间隔
+const taskScheduler = new TaskScheduler(3, 100); // 可根据需要调整并发和最小时间间隔
 
 // 注册任务
 taskScheduler.registerTask(updateTokenMetricsTask);
 taskScheduler.registerTask(updateTokenSecurityTask);
+taskScheduler.registerTask(updateTokenStatsTask);
 
 // 启动调度器
 taskScheduler.start();
 updateTokenSecurity();
+updateTokenStats();
