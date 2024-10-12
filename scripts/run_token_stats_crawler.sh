1a3486efb9fb75db8ae8adba0c31a42102f80530
0xdigiscore <0xmassis@gmail.com>
2024-10-12 17:54:06 +0800

    feat: fix script

--- scripts/run_token_stats_crawler.sh
+++ scripts/run_token_stats_crawler.sh
@@ -8,7 +8,7 @@ set +a
 while true
 do
     echo "[$(date)] 启动 @dex_crawler/gecko_crawler start:token_stats..."
-    yarn start:token_stats_crawler
+    yarn start:stats_crawler
     echo "[$(date)] 执行完成，等待 2 分钟..."
     sleep 30
 done
