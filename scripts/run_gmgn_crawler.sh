488b70a5f2342507ae08e9396a426c13c628ba2a
0xdigiscore <0xmassis@gmail.com>
2024-10-14 15:51:23 +0800

    feat: fix data

--- scripts/run_gmgn_crawler.sh
+++ scripts/run_gmgn_crawler.sh
@@ -10,5 +10,5 @@ do
     echo "[$(date)] 启动 @dex_crawler/gmgn_crawler dev..."
     yarn start:gmgn_crawler
     echo "[$(date)] 执行完成，等待 10 分钟..."
-    sleep 600 # 600 秒 = 10 分钟
+    sleep 10 # 600 秒 = 10 分钟
 done
