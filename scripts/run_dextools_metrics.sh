b738958090927c4c2778932c243eb8ca11118096
0xdigiscore <0xmassis@gmail.com>
2024-10-21 22:57:23 +0800

    feat: fix script

--- scripts/run_dextools_metrics.sh
+++ scripts/run_dextools_metrics.sh
@@ -10,5 +10,5 @@ do
     echo "[$(date)] 启动 @dex_crawler/dextools_crawler start:token_stats..."
     yarn start:dextools_crawler
     echo "[$(date)] 执行完成，等待 2 分钟..."
-    sleep 30
+    sleep 60
 done
